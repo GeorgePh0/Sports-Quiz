0077f19636cbb2f8040f849c4ec844bb29f48ae4
GeorgePh0 <george.philip0020@gmail.com>
2023-04-26 11:25:46 +0000

    Add end and restart button, style the buttons and added function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,7 @@
 const startButton = document.getElementById('start-btn');
 const nextButton = document.getElementById('next-btn');
+const endButton = document.getElementById('end-btn');
+const restartButton = document.getElementById('restart-btn');
 const questionAreaElement = document.getElementById('question-area');
 const welcomeMsgElement = document.getElementById('welcome-msg');
 const questionElement = document.getElementById('question');
@@ -12,6 +14,8 @@ nextButton.addEventListener('click', () => {
     currentQuestion++;
     setNextQuestion();
 });
+endButton.addEventListener('click', endQuiz);
+restartButton.addEventListener('click', reloadGame);
 
 function startGame() {
     console.log('Start Quiz');
@@ -56,6 +60,11 @@ function selectAnswer(sa) {
     Array.from(answerElement.children).forEach(button => {
         checkAnswer(button, button.dataset.correct);
     });
+    if (shuffleQuestion.length > currentQuestion + 1) {
+        nextButton.classList.remove('hidden');
+    } else {
+        endButton.classList.remove('hidden');
+    }
 }
 
 function checkAnswer(element, correct) {
@@ -73,17 +82,17 @@ function clearAnswer(element) {
     element.classList.remove('wrong');
 }
 
+function endQuiz() {
+    console.log("End Quiz");
+    endButton.classList.add('hidden');
+    restartButton.classList.remove('hidden');
+}
+
+function reloadGame() {
+    window.location.reload();
+}
 
 const questions = [
-    {
-        question: 'How many teams have won the Premier League?',
-        answers: [
-            { text: '5', correct: false },
-            { text: '6', correct: false },
-            { text: '7', correct: true },
-            { text: '8', correct: false }
-        ]
-    },
     {
         question: 'Which country won the first ever World Cup in 1930?',
         answers: [
@@ -94,39 +103,12 @@ const questions = [
         ]
     },
     {
-        question: 'English rock star Elton John was twice the owner of which football club?',
-        answers: [
-            { text: 'Burnley', correct: false },
-            { text: 'Cardiff', correct: false },
-            { text: 'Watford', correct: true },
-            { text: 'Brentford', correct: false }
-        ]
-    },
-    {
         question: 'Which outfield player appeared in the Champions League final in three different decades?',
         answers: [
             { text: 'Ryan Giggs', correct: true },
             { text: 'Lionel Messi', correct: false },
             { text: 'Frank Ribery', correct: false },
             { text: 'William Gallas', correct: false }
-        ]
-    },
-    {
-        question: 'In what league is the concept of a "Designated Player" a feature?',
-        answers: [
-            { text: 'Liga MX', correct: false },
-            { text: 'MLS', correct: true },
-            { text: 'Super Lig', correct: false },
-            { text: 'Cypriot First Division', correct: false }
-        ]
-    },
-    {
-        question: 'Which club is associated with Galacticos?',
-        answers: [
-            { text: 'Rayo Vallecano', correct: false },
-            { text: 'Real Sociedad', correct: false },
-            { text: 'Barcelona', correct: false },
-            { text: 'Real Madrid', correct: true }
         ]
     },
     {
@@ -148,15 +130,6 @@ const questions = [
         ]
     },
     {
-        question: 'Who scored the fastest hat-trick in the history of Ligue 2?',
-        answers: [
-            { text: 'Tino Kadewere', correct: false },
-            { text: 'Angelo Fulgini', correct: true },
-            { text: 'Rhys Healey', correct: false },
-            { text: 'Olivier Giroud', correct: false }
-        ]
-    },
-    {
         question: 'What team has been in the Northern Counties East League Division 1 for 32 Years?',
         answers: [
             { text: 'Rossington Main FC', correct: true },
@@ -172,15 +145,6 @@ const questions = [
             { text: '20', correct: false },
             { text: '21', correct: false },
             { text: '22', correct: false }
-        ]
-    },
-    {
-        question: 'How many Grand Slam tournaments are there in tennis?',
-        answers: [
-            { text: '7', correct: false },
-            { text: '6', correct: false },
-            { text: '5', correct: false },
-            { text: '4', correct: true }
         ]
     },
     {
@@ -202,42 +166,6 @@ const questions = [
         ]
     },
     {
-        question: 'What is the French Open surface?',
-        answers: [
-            { text: 'Artificial Grass', correct: false },
-            { text: 'Hard', correct: false },
-            { text: 'Grass', correct: false },
-            { text: 'Clay', correct: true }
-        ]
-    },
-    {
-        question: 'When was the French Open first held?',
-        answers: [
-            { text: '1878', correct: false },
-            { text: '1891', correct: true },
-            { text: '1895', correct: false },
-            { text: '1898', correct: false }
-        ]
-    },
-    {
-        question: 'What is the Australian Open also known as?',
-        answers: [
-            { text: 'Mega Slam', correct: false },
-            { text: 'Australian Slam', correct: false },
-            { text: 'Happy Slam', correct: true },
-            { text: 'Best Slam', correct: false }
-        ]
-    },
-    {
-        question: 'Who has won the most Australian Open titles?',
-        answers: [
-            { text: 'Novak Djokovic', correct: true },
-            { text: 'Roger Federer', correct: false },
-            { text: 'Jack Crawford', correct: false },
-            { text: 'Roy Emerson', correct: false }
-        ]
-    },
-    {
         question: 'When was the Australian Open first held in tennis?',
         answers: [
             { text: '1896', correct: false },
@@ -253,15 +181,6 @@ const questions = [
             { text: 'The Shanghai Open', correct: false },
             { text: 'The Asian Open', correct: false },
             { text: 'The Shanghai Masters', correct: true }
-        ]
-    },
-    {
-        question: 'What year was the first IPL season held in cricket?',
-        answers: [
-            { text: '2002', correct: false },
-            { text: '2004', correct: false },
-            { text: '2006', correct: false },
-            { text: '2008', correct: true }
         ]
     },
     {
@@ -292,57 +211,12 @@ const questions = [
         ]
     },
     {
-        question: 'What is the length of a cricket pitch?',
-        answers: [
-            { text: '20 Yards', correct: false },
-            { text: '22 Yards', correct: true },
-            { text: '24 Yards', correct: false },
-            { text: '26 Yards', correct: false }
-        ]
-    },
-    {
         question: 'What is the maximum weight of a cricket ball?',
         answers: [
             { text: '5.5 ounces', correct: true },
             { text: '6 ounces', correct: false },
             { text: '6.5 ounces', correct: false },
             { text: '5 ounces', correct: false }
-        ]
-    },
-    {
-        question: 'What is the height of the cricket stumps?',
-        answers: [
-            { text: '30 incehs', correct: false },
-            { text: '26 inches', correct: false },
-            { text: '28 inches', correct: true },
-            { text: '32 inches', correct: false }
-        ]
-    },
-    {
-        question: 'What is the minimum number of overs that must be bowled in a Test match in a day?',
-        answers: [
-            { text: '80', correct: false },
-            { text: '90', correct: true },
-            { text: '100', correct: false },
-            { text: '110', correct: false }
-        ]
-    },
-    {
-        question: 'How many fielders are allowed outside the 30-yard circle in a limited-overs cricket match?',
-        answers: [
-            { text: '4', correct: true },
-            { text: '5', correct: false },
-            { text: '6', correct: false },
-            { text: '7', correct: false }
-        ]
-    },
-    {
-        question: 'What does T20 stand for?',
-        answers: [
-            { text: 'Two20', correct: false },
-            { text: 'Tea20', correct: false },
-            { text: 'Twenty20', correct: true },
-            { text: 'Touch20', correct: false }
         ]
     },
     {
@@ -373,39 +247,12 @@ const questions = [
         ]
     },
     {
-        question: 'How old was Amir Khan when he won silver at the Athens 2004 Olympics?',
-        answers: [
-            { text: '16', correct: false },
-            { text: '17', correct: true },
-            { text: '18', correct: false },
-            { text: '19', correct: false }
-        ]
-    },
-    {
         question: 'Which part of Evander Holyfield did Mike Tyson bite?',
         answers: [
             { text: 'Shoulder', correct: false },
             { text: 'Nose', correct: false },
             { text: 'Neck', correct: false },
             { text: 'Ear', correct: true }
-        ]
-    },
-    {
-        question: 'What is the nickname of Tyson Fury?',
-        answers: [
-            { text: 'Gypsy', correct: false },
-            { text: 'The Gypsy King', correct: true },
-            { text: 'Fury Fire', correct: false },
-            { text: 'Big Tyson', correct: false }
-        ]
-    },
-    {
-        question: 'How many times has Anthony Joshua fought outside of the UK?',
-        answers: [
-            { text: '0', correct: false },
-            { text: '1', correct: false },
-            { text: '2', correct: true },
-            { text: '3', correct: false }
         ]
     },
     {
@@ -424,15 +271,6 @@ const questions = [
             { text: 'Sven Ottke', correct: false },
             { text: 'Floyd Mayweather', correct: false },
             { text: 'Joe Calzaghe', correct: true }
-        ]
-    },
-    {
-        question: 'In which year was Floyd Mayweathers last professional fight prior to his comeback showdown with Conor McGregor?',
-        answers: [
-            { text: '2013', correct: false },
-            { text: '2014', correct: false },
-            { text: '2015', correct: true },
-            { text: '2016', correct: false }
         ]
     }
 ];
